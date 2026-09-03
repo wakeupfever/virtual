85418371be1072dda60fbb7059f2f5579c7187fb
xl <1874803565@qq.com>
2026-09-03 17:50:23 +0800

    feat(config): 更新 vite.config.ts 以支持在所有主机上访问开发服务器

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: '.',
-  server: { port: 5173, open: false },
+  server: { port: 5173, open: false, host: '0.0.0.0' },
   appType: 'mpa',
 })
